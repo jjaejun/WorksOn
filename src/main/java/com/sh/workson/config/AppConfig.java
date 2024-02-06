825fbdf7667916b506b8b1d636653a078a015b87
jjaejun <leejj3240@gmail.com>
2024-02-06 17:34:13 +0900

    Feat: Chat 엔티티 코드 추가, 테이블 생성

---
+++ src/main/java/com/sh/workson/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.sh.workson.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ src/test/java/com/sh/workson/chat/service/ChatServiceTest.java
@@ -0,0 +1,7 @@
+package com.sh.workson.chat.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChatServiceTest {
+
+}
