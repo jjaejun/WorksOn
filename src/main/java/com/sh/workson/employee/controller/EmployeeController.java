52db625ef586608e458d33068e9f696b8b39e341
minjeong <snailmin27@gmail.com>
2024-02-07 17:15:48 +0900

    Feat: head의 csrf 토큰 발급 추가

--- src/main/java/com/sh/workson/employee/controller/EmployeeController.java
+++ src/main/java/com/sh/workson/employee/controller/EmployeeController.java
@@ -2,17 +2,25 @@ package com.sh.workson.employee.controller;
 
 import com.sh.workson.auth.service.AuthService;
 import com.sh.workson.auth.vo.EmployeeDetails;
+import com.sh.workson.employee.dto.EmployeeUpdateProfileDto;
 import com.sh.workson.employee.service.EmployeeService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/employee")
@@ -37,6 +45,18 @@ public class EmployeeController {
         log.debug("employeeDetails = {}", employeeDetails);
     }
 
+    @PostMapping("/updateProfile.do")
+    public ResponseEntity<?> updateProfile(
+            @Valid EmployeeUpdateProfileDto employeeUpdateProfileDto,
+            BindingResult bindingResult,
+            @RequestParam(name = "upFile") List<MultipartFile> upFiles,
+            @AuthenticationPrincipal EmployeeDetails employeeDetails,
+            RedirectAttributes redirectAttributes
+    ){
+        log.debug("upFiles = {}", upFiles);
+
+        return null;
+    }
 
 
 
