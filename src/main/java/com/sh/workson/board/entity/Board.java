fec57ccf88f5e7ae8102e5980e28c40d1e9ac161
hjh705 <ok9175@gmail.com>
2024-02-14 09:26:54 +0900

    Fix : 로컬용 수정 되돌림

--- src/main/java/com/sh/workson/board/entity/Board.java
+++ src/main/java/com/sh/workson/board/entity/Board.java
@@ -26,7 +26,7 @@ public class Board {
             name = "seq_board_id_generator",
             sequenceName = "seq_board_id",
             initialValue = 1,
-            allocationSize = 1)
+            allocationSize = 50)
     private Long id;
     @Enumerated(EnumType.STRING)
     private Type type;
