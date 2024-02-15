7c35d85f531e377ada2d4c24efedaf79369a418d
Seoungminjun <dkwj000@naver.com>
2024-02-16 01:14:39 +0900

    Feat: attend 페이지바 구현

--- src/main/java/com/sh/workson/attend/service/AttendService.java
+++ src/main/java/com/sh/workson/attend/service/AttendService.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class AttendService {
     @Autowired
-    AttendRepository attendRepository;
+    private AttendRepository attendRepository;
     @Autowired
     private ModelMapper modelMapper;
     @Autowired
