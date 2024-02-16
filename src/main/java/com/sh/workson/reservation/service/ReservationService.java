46e602a2c180daad7e882b87bfa1eceecbf4f455
jjaejun <leejj3240@gmail.com>
2024-02-16 16:36:39 +0900

    Update: 예약 일정 검색(비동기) 추가

--- src/main/java/com/sh/workson/reservation/service/ReservationService.java
+++ src/main/java/com/sh/workson/reservation/service/ReservationService.java
@@ -2,15 +2,20 @@ package com.sh.workson.reservation.service;
 
 import com.sh.workson.employee.entity.Employee;
 import com.sh.workson.reservation.dto.ReservationCreateDto;
+import com.sh.workson.reservation.dto.ReservationListDto;
 import com.sh.workson.reservation.entity.Reservation;
 import com.sh.workson.reservation.repository.ReservationRepository;
 import com.sh.workson.resource.entity.Resource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -45,5 +50,22 @@ public class ReservationService {
 
     public void deleteById(Long reservationId) {
         reservationRepository.deleteById(reservationId);
+    }
+
+    public Page<ReservationListDto> findBetweenSearchDate(Pageable pageable, Long resourceId, LocalDateTime startTime, LocalDateTime endTime) {
+        Page<Reservation> reservationPage = reservationRepository.findBetweenSearchDate(pageable, resourceId, startTime, endTime);
+        return reservationPage.map((reservation -> convertToReservationListDto(reservation)));
+    }
+
+    private ReservationListDto convertToReservationListDto(Reservation reservation) {
+        ReservationListDto reservationListDto = ReservationListDto.builder()
+                .startAt(reservation.getStartAt())
+                .endAt(reservation.getEndAt())
+                .content(reservation.getContent())
+                .count(reservation.getCount())
+                .empName(reservation.getEmployee().getName())
+                .resourceId(reservation.getResourceId())
+                .build();
+        return reservationListDto;
     }
 }
