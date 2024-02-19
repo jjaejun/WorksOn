301141d47afe1177622251f2a4056d2864207b96
Seoungminjun <dkwj000@naver.com>
2024-02-19 12:20:27 +0900

    Feat: 일일업무일지 Controller 을 indexController로 수정

--- src/main/java/com/sh/workson/common/controller/IndexController.java
+++ src/main/java/com/sh/workson/common/controller/IndexController.java
@@ -1,28 +1,53 @@
 package com.sh.workson.common.controller;
 
 import com.sh.workson.auth.vo.EmployeeDetails;
+import com.sh.workson.dailywork.entity.DailyWorkListDto;
+import com.sh.workson.dailywork.service.DailyWorkService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class IndexController {
+
+    @Autowired
+    private DailyWorkService dailyWorkService;
 
     @GetMapping("")
     public String index(
             Authentication authentication,
-            @AuthenticationPrincipal EmployeeDetails employeeDetails
+            @AuthenticationPrincipal EmployeeDetails employeeDetails,
+            Model model,  @PageableDefault(value = 10, page = 0) Pageable pageable
     ){
         if(employeeDetails == null){
             return "/auth/login";
         }
         else {
 
-//            민준
+//          민준
+            Long id = employeeDetails.getEmployee().getId();
 
+            int totalSeedCount = employeeDetails.getEmployee().getSeed();
+            int totalCherryCount = employeeDetails.getEmployee().getCherry();
+
+            Page<DailyWorkListDto> dailyWorkListDtoPage = dailyWorkService.findAll(pageable, id);
+
+            model.addAttribute("totalSeedCount", String.valueOf(totalSeedCount));
+            model.addAttribute("totalCherryCount", String.valueOf(totalCherryCount));
+            model.addAttribute("dailyworks", dailyWorkListDtoPage.getContent());
+            model.addAttribute("totalCount", dailyWorkListDtoPage.getTotalPages());
+
+            log.debug("dailyworks = {}", dailyWorkListDtoPage.getContent());
 
 
 
@@ -84,7 +109,7 @@ public class IndexController {
 
 
 
-//          우진             
+//          우진
 
 
 
