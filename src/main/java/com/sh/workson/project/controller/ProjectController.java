d37f41eb4ece70905ab7b02c94802430eecdcb40
minjeong <snailmin27@gmail.com>
2024-02-20 23:54:49 +0900

    Feat: 프로젝트에 등록된 업무 목록 전체보기

--- src/main/java/com/sh/workson/project/controller/ProjectController.java
+++ src/main/java/com/sh/workson/project/controller/ProjectController.java
@@ -461,4 +461,54 @@ public class ProjectController {
 
         return new ResponseEntity<>(issue.getId(), HttpStatus.OK);
     }
+
+
+    @GetMapping("projectTotalTaskList.do")
+    public void projectTotalTaskList(
+            @RequestParam("id") Long id,
+            Model model,
+            @AuthenticationPrincipal EmployeeDetails employeeDetails,
+            @RequestParam(name = "page1",defaultValue = "0") int page1,
+            @RequestParam(name = "size1", defaultValue = "5") int size1,
+            @RequestParam(name = "page2", defaultValue = "0") int page2,
+            @RequestParam(name = "size2", defaultValue = "5") int size2,
+            @RequestParam(name = "page2", defaultValue = "0") int page3,
+            @RequestParam(name = "size2", defaultValue = "10") int size3
+    ){
+        // 사원이 참여중인 프로젝트만 조회
+        Page<ProjectListDto> projects = projectService.findByEmpId(employeeDetails.getEmployee(), PageRequest.of(page2, size2));
+        // 사원이 생성한 프로젝트 조회
+        Page<ProjectListDto> projects2 = projectService.findByOwnerId(employeeDetails.getEmployee(), PageRequest.of(page1, size1));
+
+        model.addAttribute("projectEmp", projects.getContent());
+        model.addAttribute("projectEmpTotalCount", projects.getTotalElements());
+        model.addAttribute("projectEmpSize", projects.getSize());
+        model.addAttribute("projectEmpNumber", projects.getNumber());
+        model.addAttribute("projectEmpTotalpages", projects.getTotalPages());
+
+        log.debug("project = {}", projects.getContent());
+        model.addAttribute("projectOwner", projects2.getContent());
+        model.addAttribute("projectOwnerTotalCount", projects2.getTotalElements());
+        model.addAttribute("projectOwnerSize", projects2.getSize());
+        model.addAttribute("projectOwnerNumber", projects2.getNumber());
+        model.addAttribute("projectOwnerTotalpages", projects2.getTotalPages());
+
+
+        Page<TaskDetailDto> taskDetailDtos = taskService.findAllProjectTask(id, PageRequest.of(page3, size3));
+        log.debug("task = {}", taskDetailDtos);
+
+        model.addAttribute("tasks", taskDetailDtos);
+        model.addAttribute("totalCount", taskDetailDtos.getTotalElements());
+        model.addAttribute("size", taskDetailDtos.getSize());
+        model.addAttribute("number", taskDetailDtos.getNumber());
+        model.addAttribute("totalpages", taskDetailDtos.getTotalPages());
+    }
+
+
+    @GetMapping("projectTotalIssueList.do")
+    public void projectTotalIssueList(
+
+    ){
+
+    }
 }
