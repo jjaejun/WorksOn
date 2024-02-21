b7f96b16bde51db93a51591e2be9608511b9031a
minjeong <snailmin27@gmail.com>
2024-02-21 14:33:02 +0900

    Feat: 게시판 전체글, 타입별 페이징 처리 구현(attachment 조인 삭제 후 별도의 쿼리 작성)

--- src/main/java/com/sh/workson/attachment/repository/AttachmentRepository.java
+++ src/main/java/com/sh/workson/attachment/repository/AttachmentRepository.java
@@ -1,5 +1,6 @@
 package com.sh.workson.attachment.repository;
 
+import com.sh.workson.attachment.entity.AttachType;
 import com.sh.workson.attachment.entity.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +11,6 @@ public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
 
     @Query("from Attachment a join fetch a.employee e join fetch e.position join fetch e.department join fetch e.authorities where a.boardId = :id and a.type = 'PROJECT' order by a.createdAt desc")
     List<Attachment> findAllAttachmentByProjectId(Long id);
+    @Query("from Attachment a join fetch a.employee e join fetch e.position join fetch e.department join fetch e.authorities where a.boardId = :id and a.type = 'BOARD' order by a.createdAt desc")
+    List<Attachment> findByBoardId(Long id);
 }
