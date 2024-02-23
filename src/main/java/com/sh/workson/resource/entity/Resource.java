8eb811288f6ce15846489dfd844b8cf22b205728
jjaejun <leejj3240@gmail.com>
2024-02-23 10:00:34 +0900

    Fix: 내 예약 내역 자원 리스트 별로 출력 stackOverFlowError 수정

--- src/main/java/com/sh/workson/resource/entity/Resource.java
+++ src/main/java/com/sh/workson/resource/entity/Resource.java
@@ -2,10 +2,7 @@ package com.sh.workson.resource.entity;
 
 import com.sh.workson.reservation.entity.Reservation;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString(exclude = "reservations")
 public class Resource {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_tb_resource_id_generator")
